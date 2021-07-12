93eb4a1da793aac0737d7fa5124740e7df5550c5
gitty4641 <gittyweiser22@gmail.com>
2021-07-12 13:14:06 +0300

    postSchema

---
+++ model/post.js
@@ -0,0 +1,24 @@
+
+const mongoose = require('mongoose');
+
+const PostSchema = mongoose.Schema({
+
+    image: {
+        type: String
+    },
+
+    title: {
+        type: String
+    },
+    discription:{
+        type:String
+    },
+
+    magazinId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Magazin'
+    }
+
+})
+
+module.exports = mongoose.model('Post', PostSchema)
