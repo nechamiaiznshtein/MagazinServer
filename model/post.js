ccba704d412e603d5fb410213c1c99199c80540e
gitty4641 <gittyweiser22@gmail.com>
2021-07-12 13:42:54 +0300

    post schema

--- model/post.js
+++ model/post.js
@@ -6,14 +6,18 @@ const PostSchema = mongoose.Schema({
     image: {
         type: String
     },
-
     title: {
         type: String
     },
-    discription:{
-        type:String
+    discription: {
+        type: String
     },
-
+    views: {
+        type: Number
+    },
+    publishDate: {
+        type: Date
+    },
     magazinId: {
         type: mongoose.Types.ObjectId,
         ref: 'Magazin'
